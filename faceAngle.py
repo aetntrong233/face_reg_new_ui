9a929602af98e873c6c297189f6805f1f3bf59d7
aetntrong233 <aetntrong233@gmail.com>
2022-06-04 21:32:50 +0700

    fix division zero error

--- faceAngle.py
+++ faceAngle.py
@@ -62,9 +62,10 @@ def roll_angle(landmark):
     # tìm góc lệch giữa đoạn thẳng nối 2 điểm giữa 2 mắt và trục ox
     delta_x = center_left[0] - center_right[0]
     delta_y = center_left[1] - center_right[1]
-    angle = 0
-    if delta_x != 0:
+    try:
         angle = np.arctan(delta_y / delta_x)
+    except ZeroDivisionError:
+        angle = 0
     angle = (angle * 180) / np.pi
     return angle
     
